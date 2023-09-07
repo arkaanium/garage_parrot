09a8206aa99cf30ba9627df957aa2b9dff919a38
arkaanium <arkaniumfr@gmail.com>
2023-09-07 12:55:55 +0200

    added login system

---
+++ actions/login.php
@@ -0,0 +1,33 @@
+<?php
+session_start([
+    'cookie_lifetime' => 315360000,
+]);
+
+require '../includes/db.php';
+require '../includes/key.php';
+
+if(!isset($_SESSION['id'])){
+    if(isset($_POST['email']) && isset($_POST['password'])){
+        $findUser = $bdd->prepare('SELECT * FROM users WHERE email=:email');
+        $findUser->execute([ 'email' => $_POST['email'] ]);
+        $userCount = $findUser->rowCount();
+        if($userCount > 0){
+            $userInfos = $findUser->fetch();
+            if(hash_hmac('sha256',$_POST['password'],$secret) == $userInfos['password']){
+                $_SESSION['id'] = $userInfos['id'];
+                $_SESSION['name'] = $userInfos['name'];
+                $_SESSION['type'] = $userCount['type'];
+                header('Location: ../gestion?r=connected');
+            }else{
+                header('Location: login?r=invalid_password');
+            }
+        }else{
+            header('Location: login?r=unknown_user');
+        }
+    }else{
+        header('Location: login?r=incomplete_fields');
+    }
+}else{
+    header('Location: ../gestion');
+}
+?>
---
+++ login.php
@@ -0,0 +1,39 @@
+<?php
+include('includes/config.php');
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?=$site_name;?></title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+        <link rel="icon" type="image/x-icon" href="<?=$logo;?>">
+    </head>
+    <body class="text-center">
+        <div class="vh-100 d-flex justify-content-center align-items-center">
+            <form action="actions/login.php" method="post">
+                <div class="card" style="width: 30rem;">
+                    <div class="card-header"><img src="<?=$logo;?>" width="300"></div>
+                    <div class="card-body">
+                        <h3 class="card-title">Authentification</h3>
+                        <hr>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1">E-Mail</span>
+                            <input class="form-control" type="email" name="email" placeholder="Adresse e-mail" required><br>
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1">Mot de passe</span>
+                            <input class="form-control" type="password" name="password" placeholder="Mot de passe" required><br>
+                        </div>
+                        <hr>
+                        <button type="submit" class="btn btn-success">Se connecter</button>
+                    </div>
+                </div>
+                <br>
+                <a href="index" class="btn btn-sm btn-secondary">Retour à l'accueil</a>
+            </form>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    </body>
+</html>
