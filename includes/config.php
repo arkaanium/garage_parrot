4a5a19c9dcf804c5cf840be2f58b4feeddd5091f
arkaanium <arkaniumfr@gmail.com>
2023-09-07 12:55:08 +0200

    initial commit

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+// GENERAL INFORMATIONS
+$site_name = 'Garage V. Parrot';
+$logo = 'img/logo.png';
+
+// DATABASE INFORMATIONS
+$ip = 'localhost';
+$dbname = 'garage';
+$username = 'root';
+$password = '';
+?>
---
+++ includes/key.php
@@ -0,0 +1,5 @@
+<?php
+// DO NOT MODIFY
+$secret = "pwgc&%'x'#8u2Zp*YZ8u|TQD| /P<1'Uvy;up5KI+l8J_Fv+io|+Ll2=5),Yyb [w5=KHe6[#)I.-qXS$[yd@U2!/t^,/o'a@OMVI5'7Y(8Fyx.!*yFVQ(=tGpCZPbtb/IBwmhNDAk(+BK:=MVPlU)1vcC6G_o0eqasxU8VRt08xcS#4095ID.QB3jDi+tzeNsOjEroa@(PrhxJ)iDK'YdT=#-R@'(i3r'./2@pZ_SE_Wd$+{XZCW1#{1c>P2&iKKiaXPF`!cC7?sN[H{_8W1Xx647_#/-f.[*z&'[j+q!qrc<1 l";
+// DO NOT MODIFY
+?>
