106008815ebd91af5377d1d77e2d0134e4333841
arkaanium <arkaniumfr@gmail.com>
2023-09-09 02:31:21 +0200

    fix filter

--- js/catalog.js
+++ js/catalog.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
 
 function getCatalog() {
     kmMin = document.getElementById('kmMin').value;
-    kmMax = document.getElementById('kmMin').value;
-    priceMin = document.getElementById('kmMin').value;
-    priceMax = document.getElementById('kmMin').value;
-    yearMin = document.getElementById('kmMin').value;
-    yearMax = document.getElementById('kmMin').value;
+    kmMax = document.getElementById('kmMax').value;
+    priceMin = document.getElementById('priceMin').value;
+    priceMax = document.getElementById('priceMax').value;
+    yearMin = document.getElementById('yearMin').value;
+    yearMax = document.getElementById('yearMax').value;
     $.ajax({
         url: ("ajax/catalog.php?kmMin="+kmMin+"&kmMax="+kmMax+"&priceMin="+priceMin+"&priceMax="+priceMax+"&yearMin="+yearMin+"&yearMax="+yearMax),
         type: "GET",
