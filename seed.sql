1af87a7ed882c019f3ca9537a2cb8681a59af1bf
arkaanium <arkaniumfr@gmail.com>
2023-09-14 12:45:25 +0200

    fix synthax error (remove of single quotes for table and columns names)

--- seed.sql
+++ seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO 'reviews' ('id', 'author', 'rate', 'subject', 'comment', 'status', 'creation_date') VALUES
+INSERT INTO reviews (id, author, rate, subject, comment, status, creation_date) VALUES
 (1, 'Florent Damel', 5, 'Très bien', 'Je recommande', 1, '2023-09-09 17:31:33'),
 (2, 'Stephane Markcl', 5, 'Ok', 'Très bien', 1, '2023-09-09 17:31:33'),
 (3, 'Baptiste F', 5, 'Super equipe et bon suivi', "Un super garage et une équipe à l'écoute. Je recommande", 1, '2023-09-09 17:31:33'),
